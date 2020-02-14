cb549c4e6ffc8eabde7400310c9072b06d940a61
Alexandre Bustico <alexandre.bustico@enac.fr>
2020-02-14 08:58:11 +0100

    emit event on pprz message, implement itow to rtcdate

--- source/receiveBlackboard.cpp
+++ source/receiveBlackboard.cpp
@@ -18,6 +18,8 @@ namespace {
   uint8_t rx_buffer[255];
   void new_message_cb(uint8_t sender_id, uint8_t receiver_id, uint8_t class_id, uint8_t message_id, uint8_t *buf);
   uint8_t nextChar=0U;
+  static ReceiveBlackboard *rbb = nullptr; // hack waiting for user_data field in callback
+  static void rtcSetTime(uint16_t week, uint32_t itow);
 };
 
 
@@ -31,6 +33,7 @@ bool ReceiveBlackboard::initInThreadContext()
 
 bool ReceiveBlackboard::init()
 {
+  rbb = this;
   dev_rx = pprzlink_device_rx_init(
 				   [] (void) -> int { // char_available
 				     return 1;
@@ -62,17 +65,46 @@ bool ReceiveBlackboard::loop()
 
 namespace {
   
-  void new_message_cb([[maybe_unused]] uint8_t sender_id, [[maybe_unused]] uint8_t receiver_id,
-		   [[maybe_unused]] uint8_t class_id, uint8_t message_id, uint8_t *buf) {
+  void new_message_cb([[maybe_unused]] uint8_t sender_id,
+		      [[maybe_unused]] uint8_t receiver_id,
+		      [[maybe_unused]] uint8_t class_id,
+		      uint8_t message_id, uint8_t *buf) {
   // check message/class IDs to before extracting data from the messages
   if (message_id == PPRZ_MSG_ID_GPS) {
     // get data from GPS
-    int32_t east = pprzlink_get_GPS_utm_east(buf);
-    int32_t north = pprzlink_get_GPS_utm_north(buf);
-    SdCard::logSyslog(Severity::Info, "DEBUG> gps east = %ld north = %ld", east, north);
+    GpsData wdata {
+		   .mode = pprzlink_get_GPS_mode(buf),
+		   .utm_east = pprzlink_get_GPS_utm_east(buf),
+		   .utm_north = pprzlink_get_GPS_utm_north(buf),
+		   .course = pprzlink_get_GPS_course(buf),	
+		   .alt = pprzlink_get_GPS_alt(buf),		
+		   .speed = pprzlink_get_GPS_speed(buf),	
+		   .climb = pprzlink_get_GPS_climb(buf),	
+		   .week = pprzlink_get_GPS_week(buf),	
+		   .itow = pprzlink_get_GPS_itow(buf),	
+		   .utm_zone = pprzlink_get_GPS_utm_zone(buf),	
+		   .gps_nb_err = pprzlink_get_GPS_gps_nb_err(buf) 
+    };
+    rbb->blackBoard.write(wdata); // hack waiting for user_data field in callback
+    SdCard::logSyslog(Severity::Info, "DEBUG> gps east = %ld north = %ld",
+		      wdata.utm_east, wdata.utm_north);
   }
- 
-}
-
-
+  }
+  
+  static void rtcSetTime(uint16_t week, uint32_t itow)
+  {
+    if (itow != 0) {
+      // Unix timestamp of the GPS epoch 1980-01-06 00:00:00 UTC
+      constexpr uint32_t unixToGpsEpoch = 315964800;
+      struct tm time_tm;
+      time_t univTime = ((week * 7 * 24 * 3600) + (itow / 1000)) + unixToGpsEpoch;
+      gmtime_r(&univTime, &time_tm);
+      // Chibios date struct
+      RTCDateTime date;
+      rtcConvertStructTmToDateTime(&time_tm, 0, &date);
+      rtcSetTime(&RTCD1, &date);
+    }
+  }
+  
+  
 }
--- source/receiveBlackboard.hpp
+++ source/receiveBlackboard.hpp
@@ -7,12 +7,27 @@
 #include "sdcard.hpp"
 #include "hardwareConf.hpp"
 
+struct GpsData {
+  uint8_t  mode;
+  int32_t  utm_east;
+  int32_t  utm_north;
+  int16_t  course;
+  int32_t  alt;
+  uint16_t speed;
+  int16_t  climb;
+  uint16_t week;
+  uint32_t itow;
+  uint8_t  utm_zone;
+  uint8_t  gps_nb_err;   
+};
+
 class ReceiveBlackboard : public WorkerThread<TH_RECEIVEBLACKBOARD::threadStackSize,
-					   ReceiveBlackboard> {
+					      ReceiveBlackboard> {
 public:
   ReceiveBlackboard(const tprio_t m_prio) :
     WorkerThread<TH_SHOWBLACKBOARD::threadStackSize,
 		 ReceiveBlackboard>("receiveBlackboard", m_prio) {};
+  BlackBoard<GpsData, UpdateBehavior::Always> blackBoard;
 private:
   friend WorkerThread<TH_SHOWBLACKBOARD::threadStackSize, ReceiveBlackboard>;
   bool init(void) final;
