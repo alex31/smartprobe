8fd962b9a1428dc5abdca703aab4d734b83195ee
Alexandre Bustico <alexandre.bustico@enac.fr>
2020-02-27 23:50:25 +0100

    remove test code for ppzr link

--- source/receivePprzlink.cpp
+++ source/receivePprzlink.cpp
@@ -69,52 +69,26 @@ namespace {
 		      [[maybe_unused]] uint8_t receiver_id,
 		      [[maybe_unused]] uint8_t class_id,
 		      uint8_t message_id, uint8_t *buf) {
-  // check message/class IDs to before extracting data from the messages
-  if (message_id == PPRZ_MSG_ID_GPS) {
-    // get data from GPS
-    GpsData wdata {
-		   .mode = pprzlink_get_GPS_mode(buf),
-		   .utm_east = pprzlink_get_GPS_utm_east(buf),
-		   .utm_north = pprzlink_get_GPS_utm_north(buf),
-		   .course = pprzlink_get_GPS_course(buf),	
-		   .alt = pprzlink_get_GPS_alt(buf),		
-		   .speed = pprzlink_get_GPS_speed(buf),	
-		   .climb = pprzlink_get_GPS_climb(buf),	
-		   .week = pprzlink_get_GPS_week(buf),	
-		   .itow = pprzlink_get_GPS_itow(buf),	
-		   .utm_zone = pprzlink_get_GPS_utm_zone(buf),	
-		   .gps_nb_err = pprzlink_get_GPS_gps_nb_err(buf) 
-    };
-    rbb->pprzlink.write(wdata); // hack waiting for user_data field in callback
-    SdCard::logSyslog(Severity::Info, "DEBUG> gps east = %ld north = %ld",
-		      wdata.utm_east, wdata.utm_north);
-  } else if (message_id == PPRZ_MSG_ID_AEROPROBE) {
-#warning AEROPROBE receive message for DEBUG  in test_pprzlink_in_loop branch ONLY
-    int16_t velocity   = pprzlink_get_AEROPROBE_velocity(buf);  	 	
-    int16_t a_attack   = pprzlink_get_AEROPROBE_a_attack(buf); 	 		
-    int16_t a_sideslip = pprzlink_get_AEROPROBE_a_sideslip(buf);		
-    int32_t altitude   = pprzlink_get_AEROPROBE_altitude(buf);
-    int32_t dynamic_p  = pprzlink_get_AEROPROBE_dynamic_p(buf);	 	
-    int32_t static_p   = pprzlink_get_AEROPROBE_static_p(buf);	    	
-    uint8_t checksum   = pprzlink_get_AEROPROBE_checksum(buf);
-    SdCard::logSyslog(Severity::Info, "RECEIVE loopback message for AEROPROBE "
-		      "velocity   =%d; " 
-		      "a_attack   =%d; " 
-		      "a_sideslip =%d; " 
-		      "altitude   =%ld; " 
-		      "dynamic_p  =%ld; " 
-		      "static_p   =%ld; " 
-		      "checksum   =%u",
-		      velocity, 
-		      a_attack, 
-		      a_sideslip,
-		      altitude, 
-		      dynamic_p,
-		      static_p, 
-		      checksum); 
-		      
-    
-  }
+    // check message/class IDs to before extracting data from the messages
+    if (message_id == PPRZ_MSG_ID_GPS) {
+      // get data from GPS
+      GpsData wdata {
+		     .mode = pprzlink_get_GPS_mode(buf),
+		     .utm_east = pprzlink_get_GPS_utm_east(buf),
+		     .utm_north = pprzlink_get_GPS_utm_north(buf),
+		     .course = pprzlink_get_GPS_course(buf),	
+		     .alt = pprzlink_get_GPS_alt(buf),		
+		     .speed = pprzlink_get_GPS_speed(buf),	
+		     .climb = pprzlink_get_GPS_climb(buf),	
+		     .week = pprzlink_get_GPS_week(buf),	
+		     .itow = pprzlink_get_GPS_itow(buf),	
+		     .utm_zone = pprzlink_get_GPS_utm_zone(buf),	
+		     .gps_nb_err = pprzlink_get_GPS_gps_nb_err(buf) 
+      };
+      rbb->pprzlink.write(wdata); // hack waiting for user_data field in callback
+      SdCard::logSyslog(Severity::Info, "DEBUG> gps east = %ld north = %ld",
+			wdata.utm_east, wdata.utm_north);
+    } 
   }
   
   static void rtcSetTime(uint16_t week, uint32_t itow)
@@ -131,6 +105,5 @@ namespace {
       rtcSetTime(&RTCD1, &date);
     }
   }
-  
   
 }
